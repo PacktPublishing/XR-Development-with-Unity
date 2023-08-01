fb6b7f65aa6185808e35625f8fd04a72690312d2
raffaelrizzo <122754087+raffaelrizzo@users.noreply.github.com>
2023-08-01 08:51:16 +0200

    Collision Velocity added

---
+++ Chapter 07/Assets/Scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour
+{
+    public AudioClip soundClip;
+    private AudioSource _soundSource;
+    public string tag;
+
+    public bool enableVelocity = true;
+    public float minimumVelocity = 0;
+    public float maximumVelocity = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _soundSource = GetComponent<AudioSource>();
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag(tag))
+        {
+            VelocityEstimator velEstimator = other.GetComponent<VelocityEstimator>();
+            if (velEstimator && enableVelocity)
+            {
+                float v = velEstimator.GetVelocityEstimate().magnitude;
+                float soundVolume = Mathf.InverseLerp(minimumVelocity, maximumVelocity, v);
+
+                _soundSource.PlayOneShot(soundClip, soundVolume);
+            }
+            else
+                _soundSource.PlayOneShot(soundClip);
+
+        }
+    }
+}
