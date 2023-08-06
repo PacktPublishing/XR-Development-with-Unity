15ad626cd0a5194b3dbda1591965dbe6bc8bcc4d
raffaelrizzo <122754087+raffaelrizzo@users.noreply.github.com>
2023-08-06 21:21:09 +0200

    Particle System added

--- Chapter 07/Assets/Scripts/PlaySoundOnCollision.cs
+++ Chapter 07/Assets/Scripts/PlaySoundOnCollision.cs
@@ -12,31 +12,38 @@ public class PlaySoundOnCollision : MonoBehaviour
     public float minimumVelocity = 0;
     public float maximumVelocity = 3;
 
+    public ParticleSystem fogParticleSystem;  // Reference to the Particle System
+
     // Start is called before the first frame update
     void Start()
     {
         _soundSource = GetComponent<AudioSource>();
-        
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag(tag))
         {
             VelocityEstimator velEstimator = other.GetComponent<VelocityEstimator>();
-           
             if (velEstimator && enableVelocity)
             {
                 float velocityMagnitude = velEstimator.GetVelocityEstimate().magnitude;
                 float soundVolume = Mathf.InverseLerp(minimumVelocity, maximumVelocity, velocityMagnitude);
 
                 _soundSource.PlayOneShot(soundClip, soundVolume);
+
+                // Convert the velocity into an integer number of particles
+                int numParticles = Mathf.RoundToInt(velocityMagnitude * 10f);
+
+                // Create an EmitParams instance
+                ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
+
+                // Emit the particles
+                fogParticleSystem.Emit(emitParams, numParticles);
             }
             else
             {
                 _soundSource.PlayOneShot(soundClip);
-            
             }
-
         }
     }
 }
